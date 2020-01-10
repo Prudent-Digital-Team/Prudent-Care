23d297d038e3ac1034bdf1233afb2120280c7ae4
Popstar <henry.erabor@orbsolutions.com.ng>
2020-01-10 15:49:39 +0100

    modified nuxt analytics

--- www/nuxt.config.js
+++ www/nuxt.config.js
@@ -36,7 +36,15 @@ export default {
 
   plugins: ["~plugins/veevalidate"],
 
-  buildModules: ["@nuxtjs/google-analytics"],
+  // buildModules: ["@nuxtjs/google-analytics"],
+  buildModules: [
+    [
+      "@nuxtjs/google-analytics",
+      {
+        id: "UA-156000473-1"
+      }
+    ]
+  ],
   googleAnalytics: {
     id: "UA-156000473-1"
   },
