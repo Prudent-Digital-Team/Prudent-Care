71f84539955bc34602c19cc8970dfb0d2d94f873
Popstar <henry.erabor@orbsolutions.com.ng>
2020-02-03 12:17:57 +0100

    removed abouts us on the page

--- www/components/NavBar.vue
+++ www/components/NavBar.vue
@@ -34,7 +34,7 @@ export default {
   data() {
     return {
       Navigation: [
-        { name: "Abouts Us", link: "/about" },
+        { name: "About Us", link: "/about" },
         { name: "Services", link: "/services" },
         { name: "Process", link: "/process" },
         { name: "Blog", link: "/blog" },
