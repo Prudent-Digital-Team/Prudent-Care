8f21f4849cad0a8ddb82f48be89bea8cb2e6b344
Popstar <henry.erabor@orbsolutions.com.ng>
2020-02-07 16:27:48 +0100

    modified the footer links

--- www/components/Footer.vue
+++ www/components/Footer.vue
@@ -130,7 +130,7 @@ export default {
         { name: "Process", link: "process" },
         { name: "Blog", link: "blog" },
         { name: "FaQs", link: "faqs" },
-        { name: "Meal Prep", link: "meal" },
+        { name: "Meal Prep", link: "meal-prep" },
         { name: "Contact Us", link: "contact" },
         { name: "Join Us", link: "join" }
       ]
